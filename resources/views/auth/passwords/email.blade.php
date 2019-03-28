2dd5ec6af23cb18ae482db9a1badca5c50b53afa
cseshahriar <cse.shahriar.hosen@gmail.com>
2019-03-29 02:12:27 +0600

    Password Reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,113 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+        <link rel="shortcut icon" href="{{ asset('images/favicon_1.ico') }}">
 
-                <div class="card-body">
-                    @if (session('status'))
+        <title>Password Reset</title> 
+
+        <!-- Base Css Files -->
+        <link href="{{ asset('public/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+        <!-- Font Icons -->
+        <link href="{{ asset('public/assets/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('public/assets/ionicon/css/ionicons.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('public/css/material-design-iconic-font.min.css') }}" rel="stylesheet">
+
+        <!-- animate css -->
+        <link href="{{ asset('public/css/animate.css') }}" rel="stylesheet" />
+
+        <!-- Waves-effect -->
+        <link href="{{ asset('public/css/waves-effect.css') }}" rel="stylesheet">
+
+        <!-- Custom Files -->
+        <link href="{{ asset('public/css/helper.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('public/css/style.css') }}" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js') }}"></script>
+        <![endif]-->
+
+        <script src="{{ asset('public/js/modernizr.min.js') }}"></script> 
+        
+    </head>
+    <body> 
+
+        <div class="wrapper-page"> 
+            <div class="panel panel-color panel-primary panel-pages">
+
+                <div class="panel-heading bg-img"> 
+                    <div class="bg-overlay"></div>
+                    <h3 class="text-center m-t-10 text-white"> Reset Password </h3>
+                </div> 
+
+                <div class="panel-body">
+
+                @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                @endif 
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                 <form method="POST" action="{{ route('password.email') }}" role="form" class="text-center"> 
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                    @csrf
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                    <div class="alert alert-info alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                        Enter your <b>Email</b> and instructions will be sent to you!
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="form-group m-b-0"> 
+                        <div class="input-group"> 
+
+                            <input id="email" type="email" class="form-control input-lg {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required placeholder="Email">
+
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+
+                            <span class="input-group-btn"> 
+                                <button type="submit" class="btn btn-lg btn-primary waves-effect waves-light">Reset</button> 
+                            </span> 
+                        </div> 
+                    </div> 
+                    
+                </form>
+
+                </div>                                 
+                
             </div>
         </div>
-    </div>
-</div>
-@endsection
+
+        
+        <script>
+            var resizefunc = [];
+        </script>
+        <script src="{{ asset('public/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('public/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('public/js/waves.js') }}"></script>
+        <script src="{{ asset('public/js/wow.min.js') }}"></script>
+        <script src="{{ asset('public/js/jquery.nicescroll.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('public/js/jquery.scrollTo.min.js') }}"></script>
+        <script src="{{ asset('public/assets/jquery-detectmobile/detect.js') }}"></script>
+        <script src="{{ asset('public/assets/fastclick/fastclick.js') }}"></script>
+        <script src="{{ asset('public/assets/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+        <script src="{{ asset('public/assets/jquery-blockui/jquery.blockUI.js') }}"></script>
+
+
+        <!-- CUSTOM JS -->
+        <script src="{{ asset('public/js/jquery.app.js') }}"></script> 
+    
+    </body>
+</html>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,14 +1,62 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+        <link rel="shortcut icon" href="{{ asset('images/favicon_1.ico') }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
+        <title>Password Reset</title>  
+
+        <!-- Base Css Files -->
+        <link href="{{ asset('public/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+        <!-- Font Icons -->
+        <link href="{{ asset('public/assets/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('public/assets/ionicon/css/ionicons.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('public/css/material-design-iconic-font.min.css') }}" rel="stylesheet">
+
+        <!-- animate css -->
+        <link href="{{ asset('public/css/animate.css') }}" rel="stylesheet" />
+
+        <!-- Waves-effect -->
+        <link href="{{ asset('public/css/waves-effect.css') }}" rel="stylesheet">
+
+        <!-- Custom Files -->
+        <link href="{{ asset('public/css/helper.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('public/css/style.css') }}" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js') }}"></script>
+        <![endif]-->
+
+        <script src="{{ asset('public/js/modernizr.min.js') }}"></script> 
+        
+    </head>
+    <body> 
+
+        <div class="wrapper-page"> 
+            <div class="panel panel-color panel-primary panel-pages">
+
+                <div class="panel-heading bg-img"> 
+                    <div class="bg-overlay"></div>
+                    <h3 class="text-center m-t-10 text-white"> Reset Password </h3>
+                </div> 
+
+                <div class="panel-body">
+
+                @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                @endif 
+                
+                <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
@@ -57,9 +105,31 @@
                             </div>
                         </div>
                     </form>
-                </div>
+
+
+                </div>                                 
+                
             </div>
         </div>
-    </div>
-</div>
-@endsection
+
+        
+        <script>
+            var resizefunc = [];
+        </script>
+        <script src="{{ asset('public/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('public/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('public/js/waves.js') }}"></script>
+        <script src="{{ asset('public/js/wow.min.js') }}"></script>
+        <script src="{{ asset('public/js/jquery.nicescroll.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('public/js/jquery.scrollTo.min.js') }}"></script>
+        <script src="{{ asset('public/assets/jquery-detectmobile/detect.js') }}"></script>
+        <script src="{{ asset('public/assets/fastclick/fastclick.js') }}"></script>
+        <script src="{{ asset('public/assets/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+        <script src="{{ asset('public/assets/jquery-blockui/jquery.blockUI.js') }}"></script>
+
+
+        <!-- CUSTOM JS -->
+        <script src="{{ asset('public/js/jquery.app.js') }}"></script> 
+    
+    </body>
+</html>
